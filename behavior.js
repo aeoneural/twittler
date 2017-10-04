5fabe183c8400347fa1e48fb85889d6c9a9b296b
aikerim <aygerim.sauletkhan@gmail.com>
2017-10-03 21:32:01 -0700

    added button

--- behavior.js
+++ behavior.js
@@ -1,16 +1,35 @@
-     <script>
+var displayTweets = function(tweets) {
+    var $tweetDisplay = $('.tweet-display');
+    $tweetDisplay.html('');
 
-        $(document).ready(function(){
-          var $body = $('body');
-          $body.html('');
+    var index = tweets.length - 1;
+    while(index >= 0){
+      var tweet = tweets[index];
+      var $tweet = $('<div class="tweet"><span class="user"></span><span class="message"></span></div>');
+      $tweet.children('.user').text('@' + tweet.user);
+      $tweet.children('.message').text(' : ' + tweet.message);
+      $tweet.appendTo($tweetDisplay);
+      index -= 1;
+    }
+  };
 
-          var index = streams.home.length - 1;
-          while(index >= 0){
-            var tweet = streams.home[index];
-            var $tweet = $('<div></div>');
-            $tweet.text('@' + tweet.user + ': ' + tweet.message).addClass('tweets');
-            $tweet.appendTo($body);
-            index -= 1;
-          }
+/* refresh button */
+var refreshTweet = function() {
+    displayTweets(streams.home);
+  };
 
-        });
+$(document).ready(function(){
+	// jquery style
+	$('.refresh-button').on('mouseenter', function(){ 
+    	$(this).addClass('highlight');
+	})
+  	$('.refresh-button').on('mouseleave', function(){ 
+    	$(this).removeClass('highlight');
+ 	})
+ 	$('.user')
+
+  refreshTweet();
+
+  $('.refresh-button').on('click', refreshTweet);
+ 
+});
